20e1d69dcd7110a36aeb10e0e16b807eb3ccac0e
Krzysztof Mulawa <mrkristoso@gmail.com>
2021-02-10 16:39:55 +0100

    Dodaj pliki projektów.

---
+++ Ships/Ships - Online.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Ships
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Ships___Online.xaml
+    /// </summary>
+    public partial class Ships___Online : Window
+    {
+        public Ships___Online()
+        {
+            InitializeComponent();
+        }
+
+        private void PlayTile_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
